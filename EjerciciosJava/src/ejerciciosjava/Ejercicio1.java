0511d68722043f7da600c3473532230ef47fed75
crespitosoff <u20241221018@usco.edu.co>
2024-09-08 23:29:38 -0500

    varios cambios

--- EjerciciosJava/src/ejerciciosjava/Ejercicio1.java
+++ EjerciciosJava/src/ejerciciosjava/Ejercicio1.java
@@ -13,29 +13,30 @@ import java.util.Scanner;
 public class Ejercicio1 {
     
     public static void main(String[] args) {
-        int cantpc;
+        int cantPc;
         double preciou = 500;
         double preciot;
         double descuento;
         Scanner sc = new Scanner (System.in);
+        // Preguntamos la cantidad de pc's  y la guadramos en la variable cantPc
         System.out.print("Ingrese la cantidad de computadoras: ");
-        cantpc = sc.nextInt();
-        if (0 > cantpc){
+        cantPc = sc.nextInt();
+        if (0 > cantPc){
             System.out.println("Error, no se procesan cantidades negativas");
-        } else if (cantpc < 5){
+        } else if (cantPc < 5){
             System.out.println("El descuento es de: 10%");
-            descuento = (preciou * cantpc) * 0.1;
-            preciot = (preciou * cantpc) - descuento;
+            descuento = (preciou * cantPc) * 0.1;
+            preciot = (preciou * cantPc) - descuento;
             System.out.println("El precio total de la compra es de: $"+preciot);
-        } else if ((cantpc >= 5) && (cantpc <10)){
+        } else if ((cantPc >= 5) && (cantPc <10)){
             System.out.println("El descuento es de: 20%");
-            descuento = (preciou * cantpc) * 0.2;
-            preciot = (preciou * cantpc) - descuento;
+            descuento = (preciou * cantPc) * 0.2;
+            preciot = (preciou * cantPc) - descuento;
             System.out.println("El precio total de la compra es de: $"+preciot);
         } else {
             System.out.println("El descuento es de: 40%");
-            descuento = (preciou * cantpc) * 0.4;
-            preciot = (preciou * cantpc) - descuento;
+            descuento = (preciou * cantPc) * 0.4;
+            preciot = (preciou * cantPc) - descuento;
             System.out.println("El precio total de la compra es de: $"+preciot);
     }
                 
---
+++ EjerciciosJava/src/ejerciciosjava/Hola.java
@@ -0,0 +1,9 @@
+package ejerciciosjava;
+
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        
+    }
+
+}
