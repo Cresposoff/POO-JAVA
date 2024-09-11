5ac9b8f0d71ebd7fb98f8700bb278e5b107552b1
crespitosoff <u20241221018@usco.edu.co>
2024-09-11 01:13:10 -0500

    se agregó Ejercicio4.java

---
+++ EjerciciosJava/src/ejerciciosjava/Ejercicio4.java
@@ -0,0 +1,15 @@
+package ejerciciosjava;
+
+import java.util.Scanner;
+public class Ejercicio4 {
+    public static void main(String[] args){
+        double precioUnKilo;
+        double cantidadKilo;
+        double descuento;
+        double precioFinal;
+        System.out.print("Ingrese el precio del Kilo de manzanas");
+        Scanner sc = new Scanner(System.in);
+        
+
+    }
+}
