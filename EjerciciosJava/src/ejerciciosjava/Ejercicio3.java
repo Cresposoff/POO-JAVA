6f331f12dd9c98d677b86828b5a7a91e80fba07a
crespitosoff <u20241221018@usco.edu.co>
2024-09-16 09:36:32 -0500

    se mejoró Ejercicio3.java

--- EjerciciosJava/src/ejerciciosjava/Ejercicio3.java
+++ EjerciciosJava/src/ejerciciosjava/Ejercicio3.java
@@ -11,7 +11,7 @@ public class Ejercicio3 {
         int precioAparato = 0;
         double precioAparatoConIva;
         double descuentoAparato;
-        int marca = 0;
+        String marca = "Inicializada";
         double precioFinal;
         boolean condition = true;
         @SuppressWarnings("resource")
@@ -33,51 +33,47 @@ public class Ejercicio3 {
         }
         condition = true;
 
-        // Agregamos un while para que vuelva a preguntar cuando ingrese un valor no válido.
+        // Agregamos un while para que vuelva a preguntar cuando ingrese un valor no válido (que no sea tipo String o que no sea ni si ni no).
         while (condition == true) {
             System.out.println("La marca del aparato es NOSY? ");
-            System.out.println("1. Sí");
-            System.out.println("0. No");
-            marca = sc.nextInt();
+            marca = sc.nextLine();
 
-        // 517,75 Agregamos esta desición con el fin de que el usuario tenga una mejor experiencia (en vez de escribir "Sí es marca NOSY" ó "Sí/No") solamente con la pregunta ya planteada selecciona un número respectivamente.
-            if (marca == 1 || marca == 0) {
+        // Agregamos esta desición con el fin de que el usuario tenga una mejor experiencia (en vez de escribir "Sí es marca NOSY" ó "nosy") solamente con la pregunta ya planteada si/no.
+            if (marca.equalsIgnoreCase("si") || marca.equalsIgnoreCase("no")) {
                 System.out.println("¡Exelente!");
                 condition = false;
 
             } else {
-                System.out.println("Error, Ingrese un valor válido: 1 ó 0.");
+                System.out.println("Error, Ingrese un valor válido: si ó no.");
 
             }
         }
 
         precioAparatoConIva = (precioAparato * 0.19) + precioAparato;
-        if (precioAparato >= 500 && precioAparato > 0 && marca == 1){
+        if (precioAparato >= 500 && precioAparato > 0 && marca.equalsIgnoreCase("si")){
             descuentoAparato = ((precioAparatoConIva - (precioAparato * 0.1)) * 0.05);
             precioFinal = (precioAparatoConIva - (precioAparato * 0.1)) - descuentoAparato;
             System.out.println("Como el aparato cuesta igual o más a U$500 y es marca NOSY. El precio final es de: U$"+precioFinal);
             
-        } else if (precioAparato >= 500 && precioAparato > 0 && marca == 0){
+        } else if (precioAparato >= 500 && precioAparato > 0 && marca.equalsIgnoreCase("no")){
             descuentoAparato = (precioAparatoConIva - (precioAparato * 0.1));
             precioFinal = descuentoAparato;
             System.out.println("Como el aparato cuesta igual o más a U$500 y NO es marca NOSY. El precio final es de: U$"+precioFinal);
 
-        } else if (precioAparato <= 500 && precioAparato > 0 && marca == 1){
+        } else if (precioAparato <= 500 && precioAparato > 0 && marca.equalsIgnoreCase("si")){
             descuentoAparato = (precioAparatoConIva * 0.05);
             precioFinal = precioAparatoConIva - descuentoAparato;
             System.out.println("Como el aparato cuesta menos de U$500 y es marca NOSY. El precio final es de: U$"+precioFinal);
 
-        } else if (precioAparato <= 500 && precioAparato > 0 && marca == 0){
+        } else if (precioAparato <= 500 && precioAparato > 0 && marca.equalsIgnoreCase("no")){
             descuentoAparato = precioAparatoConIva;
             precioFinal = descuentoAparato;
             System.out.println("Como el aparato cuesta menos de U$500 y NO es marca NOSY. El precio final es de: U$"+precioFinal);
 
         }
-
     }
 }
 /**Pendiente:
- * Agregar gestión de cadenas.
  * Mirar si se puede mejorar las desiciones.
  * Agregar el vector (Array, donde guarde los porcentajes -> (19%), (10%), (5%)).
  * */
