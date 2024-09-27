d353128958e190f819c6b33192f93362456fa831
crespitosoff <u20241221018@usco.edu.co>
2024-09-26 23:27:18 -0500

    sdaf

--- EjerciciosJava/src/ejerciciosjavaanexo1/EjercicioArray10.java
+++ EjerciciosJava/src/ejerciciosjavaanexo1/EjercicioArray10.java
@@ -5,8 +5,16 @@ import java.util.Random;
 public class EjercicioArray10 {
     public static void main(String[] args) {
         Random alea = new Random();
-        double [] Array = new double[alea.nextDouble(10,0)+1,0];
-        for (int i = 0; i < Array.length; i++) {
+        double [] array = new double[alea.nextInt(10)+1];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = alea.nextDouble();
+
+            if (i == 0) {
+                System.out.println("El primer elemento es: "+array[0]);
+                
+            } else if (i == (array.length-1)) {
+                System.out.println("El último elemento es: "+array[(array.length-1)]);
+            }
             
         }
     }
